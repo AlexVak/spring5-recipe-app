9ccf64d8a503ff1a2a7e435987778e3d624da017
AlexVak <avakarchuk@me.com>
2019-01-21 15:46:42 +0200

    lombok implemented

--- src/main/java/com/alexvak/spring5recipeapp/services/RecipeServiceImpl.java
+++ src/main/java/com/alexvak/spring5recipeapp/services/RecipeServiceImpl.java
@@ -2,11 +2,13 @@ package com.alexvak.spring5recipeapp.services;
 
 import com.alexvak.spring5recipeapp.domain.Recipe;
 import com.alexvak.spring5recipeapp.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService {
 
@@ -18,6 +20,7 @@ public class RecipeServiceImpl implements RecipeService {
 
     @Override
     public Set<Recipe> getRecipes() {
+        log.debug("I'm in a service");
         Set<Recipe> recipes = new HashSet<>();
         recipeRepository.findAll().forEach(recipes::add);
         return recipes;
